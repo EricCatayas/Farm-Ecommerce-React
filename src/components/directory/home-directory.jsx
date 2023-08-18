2d396738599bd9eef642547e1a439bbb8209432d
EricCatayas <catayasericjay@gmail.com>
2023-08-18 18:35:03 +0800

    Modified Product Categories and Sub Categories

--- src/components/directory/home-directory.jsx
+++ src/components/directory/home-directory.jsx
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import { useState, useEffect, useContext } from 'react';
 import ProductCategories from '../category-container/product-categories';
 import ProductSearchFilter from '../search-filter/products-search-filter';
 import ProductsList from '../products/products-list';
@@ -6,42 +6,35 @@ import GoogleMapLocations from '../map/google-map-locations';
 import MainMenu from '../main-menu/main-menu';
 //mock data
 import { ProductsContext } from '../../contexts/products.context';
-import categories_data from '../../categories-data.json';
 
-class HomeDirectory extends Component {
-    static contextType = ProductsContext;
+import productsList from '../../products-data.json';
 
-    constructor(){
-        super();
-        this.state = {
-            products: [],
-            product_categories : categories_data
-          }
+const  HomeDirectory = () => {
+
+    const { products, setProducts } = useContext(ProductsContext);
+
+    useEffect(()=>{
+        setProducts(productsList);
+    }, [])
+
+    const categorySelectEventHandler = (event) => {
+        console.log("Target: " + event.target);
     }
+    
+    console.log("HomeDirectory is rendered");
 
-    componentDidMount(){
-        const { products } = this.context;
-        this.setState(()=>{
-                return {
-                    products : products
-                }
-            })
-    }
-
-    render(){
-
-        return (
-            <div className="home">
-                <div className='container'>
-                <MainMenu/>
-                <ProductCategories categories={this.state.product_categories}/>
-                <GoogleMapLocations/>
-                <ProductSearchFilter/>
-                <ProductsList products={this.state.products}/>
-                </div>
+    return (
+        <div className="home">
+            <div className='container'>
+            <MainMenu/>
+            <ProductCategories onCategorySelectEvent={categorySelectEventHandler}/>
+            <GoogleMapLocations/>
+            <ProductSearchFilter/>
+            <ProductsList products={products}/>
             </div>
-        );
-    }
+        </div>
+    );
+    
 }
 
 export default HomeDirectory;
