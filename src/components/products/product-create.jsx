7691216f60729a8ba30d54fcb1773d4ef887e3af
EricCatayas <catayasericjay@gmail.com>
2023-08-21 14:23:02 +0800

    onCategoriesEventHandler update fix

--- src/components/products/product-create.jsx
+++ src/components/products/product-create.jsx
@@ -3,21 +3,20 @@ import ProductCategories from "../category-container/product-categories";
 import { DefaultPanel } from "../panel/panel";
 import { formDataPostRequestAsync } from "../../utils/form.utils";
 
-const defaultFormFields = {
-    productName : '',
-    description : '',
-    price: '',
-    category_Id : '',
-    per_Qty_Type:'',
-    qty_In_Stock : '',
-    is_Negotiable: true,
-    image_File : null,
+const productCreateFormFields = {
+    ProductName : '',
+    Description : '',
+    Price: null,
+    Category_Id : null,
+    Per_Qty_Type: '',
+    Qty_In_Stock : null,
+    Is_Negotiable: false,
+    Image_File : null,
 }
-let selectedCategory = null;
 
 const ProductCreate = () =>  {
     
-    const [formFields, setFormFields] = useState(defaultFormFields);
+    const [formFields, setFormFields] = useState(productCreateFormFields);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -28,14 +27,13 @@ const ProductCreate = () =>  {
                 formData.append(key, formFields[key]);
             }
             const response = await formDataPostRequestAsync(formData, 
-                "/api/v1/Product/Create",
+                "/api/v1/Products/Create",
                 (data) => { 
                     console.log("Product Create response:\n"); 
                     console.log(data);
                 },
-                (error)=>{
-                    console.log("An error occured in product post request");
-                    console.log(error);
+                (error)=>{ 
+                    console.error(error);
                 });              
         }
         catch(error){
@@ -43,20 +41,26 @@ const ProductCreate = () =>  {
         }
     }
 
-    const inputChangeHandler = async (event) => {
+    const textInputChangeHandler = async (event) => {
         const { name, value } = event.target;
-        console.log(event);
         setFormFields({...formFields, [name]:value});
+    }
+    const numberInputChangeHandler = async (event) => {
+        const { name, value } = event.target;
+        const numValue = parseInt(value); 
+        setFormFields({...formFields, [name]:numValue});
     }
     const checkBoxChangeHandler = async (event) => {
         const { name, checked } = event.target;
         setFormFields({...formFields, [name]:checked});
     }
-
     const categorySelectEventHandler = (event) => {
         const { value, innerText } = event.target;
-        setFormFields({...formFields, ['category_Id']:value});
-        selectedCategory = innerText;
+        const categoryId = parseInt(value); 
+        setFormFields((prevFormFields) => ({
+            ...prevFormFields,
+            Category_Id: categoryId,
+          }));
     }
     console.log("Form Fields in Product Create");
     console.log(formFields);
@@ -65,16 +69,16 @@ const ProductCreate = () =>  {
         <section className="product-create-container">
             <form onSubmit={handleSubmit}>
                 <DefaultPanel heading={"1. Category"}/>
-                <div>{ selectedCategory ? selectedCategory : "Select Category" }</div>
+                <div>Select Category</div>
                 <ProductCategories onCategorySelectEvent={categorySelectEventHandler}/>
                 <DefaultPanel heading={"2. Product"}/>
-                <input type="text" name='productName' placeholder="Name" required onChange={inputChangeHandler} />
-                <input type="text" name='description' placeholder="Description" onChange={inputChangeHandler} />
-                <input type="text" name='price' placeholder="Price" required onChange={inputChangeHandler} />
-                <input type="text" name='per_Qty_Type' placeholder="Sold Per Qty" required onChange={inputChangeHandler} />
+                <input type="text" name='ProductName' placeholder="Name" required onChange={textInputChangeHandler} />
+                <input type="text" name='Description' placeholder="Description" onChange={textInputChangeHandler} />
+                <input type="number" name='Price' placeholder="Price" required onChange={numberInputChangeHandler} />
+                <input type="text" name='Per_Qty_Type' placeholder="Sold Per Qty" required onChange={textInputChangeHandler} />
                 <label>Is Negotiable?</label>
-                <input type="checkbox" name='is_Negotiable' placeholder="Negotiable" onChange={checkBoxChangeHandler} />
-                <input type="text" name='qty_In_Stock' placeholder="Qty In Stock" onChange={inputChangeHandler} />
+                <input type="checkbox" name='Is_Negotiable' placeholder="Negotiable" onChange={checkBoxChangeHandler} />
+                <input type="number" name='Qty_In_Stock' placeholder="Qty In Stock" onChange={numberInputChangeHandler} />
 
                 <input className="button" type="submit" value="Add Product" />
             </form>
