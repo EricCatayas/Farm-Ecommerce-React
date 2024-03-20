8f7bc0af2339807fb7f2658827870ae7057425fa
EricCatayas <catayasericjay@gmail.com>
2024-03-20 09:33:22 +0800

    Renamed and migrated request.utils.ts to typescript getRequest.utils.ts

---
+++ src/utils/getRequest.utils.ts
@@ -0,0 +1,34 @@
+export const defaultGetRequestAsync = <T>(
+  endpoint: string,
+  callback?: (response: T) => void,
+  errorCallback?: (error: Error) => void
+): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    const url = `${process.env.REACT_APP_FARM_ECOMMERCE_URL}${endpoint}`;
+
+    fetch(url, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          const errorMessages = response.statusText;
+          throw new Error(errorMessages);
+        }
+        return response.json();
+      })
+      .then((jsonResponse: T) => {
+        if (callback && typeof callback === "function") 
+          callback(jsonResponse);
+        resolve(jsonResponse);
+      })
+      .catch((error) => {
+        if (errorCallback && typeof errorCallback === "function")
+          errorCallback(error);
+        reject(error);
+      });
+  });
+};
