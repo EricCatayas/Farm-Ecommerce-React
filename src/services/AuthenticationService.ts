c174cfbac038e4143c2d22fcf33950931232a70c
EricCatayas <catayasericjay@gmail.com>
2024-03-20 14:29:24 +0800

    Moved and renamed authenticationService.js to AuthenticationService.ts

--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -1,11 +1,13 @@
-import { defaultPostRequestAsync} from "../utils/form.utils";
+import { IAuthenticationService } from "../service contracts/IAuthenticationService";
+import { defaultPostRequestAsync} from "../utils/postRequest.utils";
 import { getCookie, deleteCookie } from "../utils/cookie.utils";
 import { removeBearerPrefixInToken } from "../utils/auth.utils";
+import { User } from "../models/User";
 
-class AuthenticationService {
+class AuthenticationService implements IAuthenticationService {
   constructor() {}
 
-  async signInAsync(email, password, rememberMe) {
+  async signInAsync(email:string, password:string, rememberMe:boolean): Promise<User> {
     const formData = {
       email,
       password,
@@ -15,8 +17,8 @@ class AuthenticationService {
     return await defaultPostRequestAsync(
       formData,
       `/api/v1/Account/Login?RememberMe=${formData.rememberMe}`,
-      (data) => {
-        this.#setAuthCookies(data);
+      (response) => {
+        this.#setAuthCookies(response);
         //LOG
         console.log("Here is your sign in cookie:");
         console.log(getCookie("authorization"));
@@ -25,14 +27,18 @@ class AuthenticationService {
   }
 
   //TODO: move behavior to different module
-  async signInWithTokenAsync() {
+  async signInWithTokenAsync(): Promise<User> {
     const tokenValue = getCookie("authorization");
+    if(tokenValue == null)
+      throw new Error("User session/cookie does not exist.");
+
     const token = removeBearerPrefixInToken(tokenValue);
     
-    return await defaultPostRequestAsync(
+    return await defaultPostRequestAsync<User>(
+      null,
       `/api/v1/Account/LoginWithToken?token=${token}`,
-      (data) => {
-        this.#setAuthCookies(data);
+      (response) => {
+        this.#setAuthCookies(response);
         //LOG
         console.log("Here is your new sign in cookie:");
         console.log(getCookie("authorization"));
@@ -40,16 +46,16 @@ class AuthenticationService {
     );
   }
 
-  #setAuthCookies(data){
+  #setAuthCookies(response: User): void{
     document.cookie = `authorization=Bearer ${encodeURIComponent(
-      data.token
-    )}; expires=${data.expiration}`;
+      response.token
+    )}; expires=${response.expiration}`;
     document.cookie = `refreshToken=${encodeURIComponent(
-      data.refreshToken
-    )}; expires=${data.refreshTokenExpirationDateTime}`;
+      response.refreshToken
+    )}; expires=${response.refreshTokenExpirationDateTime}`;
   }
 
-  signOut() {
+  signOut():void {
     deleteCookie("authorization");
     deleteCookie("refreshToken");
   }
