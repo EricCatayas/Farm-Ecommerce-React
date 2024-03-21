6ffa2759a305f4cc4496d9be210e345c401ad2ee
EricCatayas <catayasericjay@gmail.com>
2024-03-21 09:05:20 +0800

    migrated cookie.utils to typescript

--- src/utils/cookie.utils.ts
+++ src/utils/cookie.utils.ts
@@ -1,5 +1,5 @@
 
-export function getCookie(name) {
+export function getCookie(name:string): string | null {
     const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
     if (match) {
       return decodeURIComponent(match[2]);
@@ -7,6 +7,6 @@ export function getCookie(name) {
     return null;
 }
 
-export function deleteCookie(name) {
+export function deleteCookie(name): void {
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
