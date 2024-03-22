b953d4b0ab11716bd64844f13d196290ef5c1c83
EricCatayas <catayasericjay@gmail.com>
2024-03-22 10:25:24 +0800

    Migrated store to typescript

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,28 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
+import { persistReducer, persistStore, PersistConfig } from "redux-persist";
 import createSagaMiddleware from "redux-saga";
 import storageSession from "redux-persist/lib/storage/session";
 import logger from "redux-logger";
-import rootReducer from "./root-reducer";
-import rootSaga from "./root-saga";
+import {rootReducer, RootReducer} from "./root.reducer";
+import rootSaga from "./root.saga";
+
 // All Redux code lives here
 // Where state lives, where actions are dispatched
 
-const sagaMiddleware = createSagaMiddleware();
+export type RootState = ReturnType<typeof rootReducer>;
 
-const persistConfig = {
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+
+const persistConfig: ExtendedPersistConfig = { 
   key: "root",
   storage: storageSession,
+  whitelist: []
   // blacklist: ['user']
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer<RootReducer>(persistConfig, rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({thunk:false}).concat(sagaMiddleware, logger),
 });
+
 
 sagaMiddleware.run(rootSaga);
 
