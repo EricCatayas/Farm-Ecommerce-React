d0b8f4c2510721d9d0d466f3ca1e1775bc63c928
EricCatayas <catayasericjay@gmail.com>
2024-03-22 17:24:18 +0800

    Migrated User saga to typescript

--- src/redux/user/user.saga.ts
+++ src/redux/user/user.saga.ts
@@ -2,24 +2,32 @@ import { takeLatest, all, call, put } from "redux-saga/effects";
 import { USER_ACTION_TYPES } from "./user.types";
 import { signInSuccess, signInFailed, signInWithTokenSuccess, signInWithTokenFailed } from "./user.actions";
 import AuthenticationService from "../../services/AuthenticationService";
+import { SagaIterator } from "redux-saga";
+import { PayloadAction } from "@reduxjs/toolkit";
 
 
 
-export function* signInAsync({ payload: { email, password, rememberMe } }) {
+export function* signInAsync(action: PayloadAction<{ email: string, password: string, rememberMe: boolean}>): SagaIterator {
   try {
     const authService = new AuthenticationService();
+    const { email, password, rememberMe } = action.payload;
     const data = yield call(authService.signInAsync, email, password, rememberMe );
 
     //LOG
     console.log(`You have been authen: ${data}`);
     
     yield put(signInSuccess(data));
-  } catch (error) {
-    yield put(signInFailed(error));
+  } catch (error: any) {
+    if (error instanceof Error) {
+      yield put(signInFailed(error));
+    }
+    else {
+      yield put(signInFailed(new Error(`An error occured: ${error}`)))
+    }
   }
 }
 
-export function* signInWithTokenAsync(){
+export function* signInWithTokenAsync(): SagaIterator{
   try{
     //LOG
     console.log("signInUserWithToken in Saga");
@@ -28,18 +36,18 @@ export function* signInWithTokenAsync(){
     console.log(data);
     yield put(signInWithTokenSuccess(data));
   }
-  catch(error){
-    yield put(signInWithTokenFailed(error))
+  catch(error: any){
+    yield put(signInWithTokenFailed(error as Error));        
   }
 }
 
 //  "ACTION LISTENER" Functions
 
-export function* onSignInAsync(){
+export function* onSignInAsync(): SagaIterator{
     yield takeLatest(USER_ACTION_TYPES.SIGN_IN_START, signInAsync);
 }
 
-export function* onSignInWithTokenAsync(){
+export function* onSignInWithTokenAsync(): SagaIterator{
     yield takeLatest(USER_ACTION_TYPES.SIGN_IN_WITH_TOKEN_START, signInWithTokenAsync);
 }
 
