0647eaf8348d415e34440c42a037af263b94c318
EricCatayas <catayasericjay@gmail.com>
2024-03-22 17:25:56 +0800

    Migrated productsListPagination saga to typescript

--- src/redux/productsListPagination/productsListPagination.saga.ts
+++ src/redux/productsListPagination/productsListPagination.saga.ts
@@ -2,8 +2,14 @@ import { takeLatest, all, call, put, select } from "redux-saga/effects";
 import { fetchProductsSuccess, fetchProductsFailed, fetchFilteredProductsSuccess, fetchFilteredProductsFailed } from "./productsListPagination.action";
 import ProductsService from "../../services/ProductsService";
 import { PRODUCTS_LIST_PAGINATION_ACTION_TYPES } from "./productsListPagination.types";
+import { SagaIterator } from "redux-saga";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
-export function* fetchProductsAsync(){
+export function* fetchProductsAsync(): SagaIterator{
+    //LOG
+    console.log("fetchProductsAsync saga");
+
     try{
         const productsService = new ProductsService();
         const productsListPagination = yield select(state => state.productsListPagination);
@@ -12,56 +18,56 @@ export function* fetchProductsAsync(){
         const data = yield call(productsService.fetchPaginatedProductsAsync, pageNumber, pageSize);
         yield put(fetchProductsSuccess(data));
     }
-    catch(error){
+    catch(error: any){
         yield put(fetchProductsFailed(error));
     }
 }
 
-export function* fetchFilteredProductsAsync({ payload: { query }}) {
+export function* fetchFilteredProductsAsync(action: PayloadAction<string>): SagaIterator{
   try {
     const productsService = new ProductsService();
 
     const data = yield call(
       productsService.fetchFilteredProductsAsync,
-      query
+      action.payload
     );
     //LOG
     console.log("Filtered Products:" + JSON.stringify(data));
 
     yield put(fetchFilteredProductsSuccess(data));
-  } catch (error) {
+  } catch (error: any) {
     yield put(fetchFilteredProductsFailed(error));
   }
 }
 
 // LISTENERS
 
-export function* onFetchProducts(){
+export function* onFetchProducts(): SagaIterator{
     yield takeLatest(
         PRODUCTS_LIST_PAGINATION_ACTION_TYPES.FETCH_PRODUCTS_START,
         fetchProductsAsync
     );
 }
-export function* onFetchFilteredProducts(){
+export function* onFetchFilteredProducts(): SagaIterator{
     yield takeLatest(
         PRODUCTS_LIST_PAGINATION_ACTION_TYPES.FETCH_FILTERED_PRODUCTS_START,
         fetchFilteredProductsAsync
     );
 }
-export function* onFetchNextPageProducts(){
+export function* onFetchNextPageProducts(): SagaIterator{
     yield takeLatest(
         PRODUCTS_LIST_PAGINATION_ACTION_TYPES.INCREMENT_PAGE,
         fetchProductsAsync
     );
 }
 
-export function* onFetchPreviousPageProducts(){
+export function* onFetchPreviousPageProducts(): SagaIterator{
     yield takeLatest(
         PRODUCTS_LIST_PAGINATION_ACTION_TYPES.DECREMENT_PAGE,
         fetchProductsAsync
     );
 }
-export function* onFetchProductsByPageNumber(){
+export function* onFetchProductsByPageNumber(): SagaIterator{
     yield takeLatest(
         PRODUCTS_LIST_PAGINATION_ACTION_TYPES.SET_PAGE_NUMBER,
         fetchProductsAsync
@@ -70,7 +76,7 @@ export function* onFetchProductsByPageNumber(){
 
 // SAGA
 
-export function* productsListPaginationSaga(){
+export function* productsListPaginationSaga(): SagaIterator{
     yield all([
         call(onFetchProducts),
         call(onFetchFilteredProducts), 
