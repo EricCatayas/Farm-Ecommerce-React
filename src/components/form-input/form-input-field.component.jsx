5815d1fe553fd420649a1dd7922856e2a9230eed
EricCatayas <catayasericjay@gmail.com>
2024-04-09 15:34:15 +0800

    Added Checkbox component

--- src/components/form-input/form-input-field.component.jsx
+++ src/components/form-input/form-input-field.component.jsx
@@ -10,3 +10,39 @@ export const FormInputField = ({icon_name, inputOptions}) => {
         </div>
     )
 }
+
+export const FormInputGroupText = ({ label, placeholder, name, onChangeHandler }) => {
+  return (
+    <div className="input-group">
+      <span className="input-group-text">{label}</span>
+      <input
+        type="text"
+        aria-label={label}
+        className="form-control"
+        name={name}
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
+
+export const Checkbox = ({ label, name, onChangeHandler }) => {
+
+  const onCheck = (event) => {
+    const { checked } = event.target;
+    event.target.value = checked;
+    if(onChangeHandler)
+      onChangeHandler(event);
+  }
+
+  return(
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" name={name} id="checkDefault" onChange={onCheck}/>
+        <label class="form-check-label" for="checkDefault">
+          {label}
+        </label>
+    </div>
+    
+  );
+}
