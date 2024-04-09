80b65117717dda0ae9ae757ce3210981462bf82f
EricCatayas <catayasericjay@gmail.com>
2024-04-09 13:24:27 +0800

    Added CategoriesListCard to ProductCategories component

--- src/components/category-container/product-categories.component.jsx
+++ src/components/category-container/product-categories.component.jsx
@@ -1,24 +1,23 @@
-import "./product-categories.styles.scss";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import SubCategories from "./product-subcategories.component";
-import CategoryMiniCard from "./category-mini-card.component";
+import CategoryListCard from "./category-list-card.component";
 import Spinner from "../spinner/spinner.component";
+import "./product-categories.styles.scss";
 
 const ProductCategories = ({ onSelectEventHandler }) => {  
   const { productCategories, isLoading, errors } = useSelector((state) => state.productCategories);
   const [clickedParentCategory, setClickedParentCategory] = useState(null);
 
-  const onClickEventHandler = (event) => {
-    const id = event.target.dataset.id;
-    const clickedParentCategory = productCategories.find((cat) => cat.id == id);
-
-    console.log("category clicked event");
+  const onClickEventHandler = (event) => {    
+    const categoryId = event.target.dataset.id;
+    console.log("On Category Clicked: " + categoryId);
+    const clickedParentCategory = productCategories.find((cat) => cat.id == categoryId);
 
     if (clickedParentCategory != null) {
       setClickedParentCategory(clickedParentCategory);
     }
-  };
+  };  
 
   const onCloseEventHandler = () => {
     setClickedParentCategory(null);
@@ -33,19 +32,12 @@ const ProductCategories = ({ onSelectEventHandler }) => {
     <section className="product-categories-container">
       <div className="row">
         <div className="col-xs-12">
-          <ul className="nav navbar-nav justify-content-evenly d-flex flex-row">
-            {!isLoading && productCategories ? (
-              productCategories.map((category) => (
-                <CategoryMiniCard
-                  key={category.id}
-                  category={category}
-                  onClickHandler={onClickEventHandler}
-                />
-              ))
-            ) : (
-              <Spinner />
-            )}
-          </ul>
+        {/* TODO: fix */}
+          {!isLoading && productCategories ? (
+            <CategoryListCard categories={productCategories} onClickHandler={onClickEventHandler}/>          
+              ) : (
+            <Spinner />
+          )}          
           {clickedParentCategory ? (
             <SubCategories
               category_id={clickedParentCategory.id}
