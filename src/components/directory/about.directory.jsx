e0c4d391e2a995cb060dcfb755bdde953dc150db
EricCatayas <catayasericjay@gmail.com>
2024-04-12 05:56:32 +0800

    Added MainMenu component to AboutUsDirectory

--- src/components/directory/about.directory.jsx
+++ src/components/directory/about.directory.jsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import AboutUs from "../about/about-us.component";
+import MainMenu from "../main-menu/main-menu.component";
 
 class AboutUsDirectory extends Component {
 
   render() {
     return (
       <section className="about-us">
+        <MainMenu/>
         <AboutUs/>
       </section>
     );
