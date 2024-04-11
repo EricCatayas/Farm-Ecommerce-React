476aca1a0719e97d303c49a163383ebde60a397a
EricCatayas <catayasericjay@gmail.com>
2024-04-12 07:21:33 +0800

    Renamed class to className in AboutUs component

--- src/components/about/about-us.component.jsx
+++ src/components/about/about-us.component.jsx
@@ -2,22 +2,22 @@ import "./about-us.styles.css";
 
 const AboutUs = () => {
     return (
-        <div class="container">
-            <div class="row about-section">
-                <div class="col-lg-3">
-                    <div class="about-img">
+        <div className="container">
+            <div className="row about-section">
+                <div className="col-lg-3">
+                    <div className="about-img">
                         <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-1.jfif" alt="Left Image 1"/>
                         <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-2.jfif" alt="Left Image 2"/>
                     </div>
                 </div>
-                <div class="col-lg-6 about-content">
-                    <img src="logo.png" alt="Farm Ecommerce Logo" class="mb-4"/>
-                    <h1 class="mb-4">Agro-Market24</h1>
+                <div className="col-lg-6 about-content">
+                    <img src="logo.png" alt="Farm Ecommerce Logo" className="mb-4"/>
+                    <h1 className="mb-4">Agro-Market24</h1>
                     <p>Agro-Market24 is a modern International agricultural exchange. We gather the agricultural and fruit
                         growing community in one place. In Agro-Market24 producers and buyers can acquire commercial contacts
                         and cooperate in an easy and fast way. You can add and view advertisement, contact other users via
                         messenger and carry out transactions.</p>
-                    <h2 class="text-center mt-5">Who uses Agro-Market24?</h2>
+                    <h2 className="text-center mt-5">Who uses Agro-Market24?</h2>
                     <ul>
                         <li>Fruit growers</li>
                         <li>Farmers</li>
@@ -27,8 +27,8 @@ const AboutUs = () => {
                         <li>Agricultural products wholesalers</li>
                     </ul>
                 </div>
-                <div class="col-lg-3">
-                    <div class="about-img">
+                <div className="col-lg-3">
+                    <div className="about-img">
                         <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-4.jfif" alt="Right Image 1"/>
                         <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-6.jfif" alt="Right Image 2"/>
                     </div>
