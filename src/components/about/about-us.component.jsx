dfe8e3debc6d7f1fcc8d636da58ac61167eb10b3
EricCatayas <catayasericjay@gmail.com>
2024-04-12 14:16:38 +0800

    AboutUs component added landing image design

--- src/components/about/about-us.component.jsx
+++ src/components/about/about-us.component.jsx
@@ -5,8 +5,12 @@ const AboutUs = () => {
         <section className="about-section">
             <div className="container">
                 <div className="row">
+                    <div className="col-12 col-lg-6 order-lg-2">
+                        <div className="about-img">
+                            <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-landing.jfif" alt="Right Image 1"/>
+                        </div>
+                    </div>
                     <div className="col-12 col-lg-6 order-lg-1 about-content">
-                        <img src="logo.png" alt="Farm Ecommerce Logo" className="mb-4"/>
                         <h1 className="mb-4">Agro-Market24</h1>
                         <p>Agro-Market24 is a modern International agricultural exchange. We gather the agricultural and fruit
                             growing community in one place. In Agro-Market24 producers and buyers can acquire commercial contacts
@@ -21,18 +25,6 @@ const AboutUs = () => {
                             <li>Importers of agricultural products</li>
                             <li>Agricultural products wholesalers</li>
                         </ul>
-                    </div>
-                    <div className="col-6 col-lg-3 order-lg-2 about-images">
-                        <div className="about-img">
-                            <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-1.jfif" alt="Left Image 1"/>
-                            <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-2.jfif" alt="Left Image 2"/>
-                        </div>
-                    </div>
-                    <div className="col-6 col-lg-3 about-images">
-                        <div className="about-img">
-                            <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-4.jfif" alt="Right Image 1"/>
-                            <img src="https://unlimitedworks.blob.core.windows.net/farmecommerce/about-us-image-6.jfif" alt="Right Image 2"/>
-                        </div>
                     </div>
                 </div>
             </div>
