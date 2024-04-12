48363068e71350051c4316247fb992e327adc078
EricCatayas <catayasericjay@gmail.com>
2024-04-12 08:48:21 +0800

    Added div.container parent element in MainMenu component

--- src/components/main-menu/main-menu.component.jsx
+++ src/components/main-menu/main-menu.component.jsx
@@ -6,19 +6,21 @@ const MainMenu = () => {
 
     return(
         <section className="main-menu text-center">
-            <div className="row justify-content-around">
-                <div className="col col-xs-3">
-                    <div className="main-menu-logo-container">
-                        <img src="/logo192.png"/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-xs-3">
+                        <div className="logo-container">
+                            <img src="/logo192.png"/>
+                        </div>
                     </div>
-                </div>
-                <div className="col col-xs-9 mobile-hide">
-                    <div className="main-menu-nav-container">
-                        <ul className="nav navbar-nav d-flex flex-row">
-                            <NavLink name={"Advertisements"} href={"/"} altHref={"#products-list-container"}/>
-                            <NavLink name={"About Us"} href={"/about-us"}/>
-                            <NavLink name={"Manage Store"} href={"/store"}/>
-                        </ul>
+                    <div className="col col-xs-9 mobile-hide">
+                        <div className="nav-container">
+                            <ul className="nav navbar-nav d-flex flex-row">
+                                <NavLink name={"Advertisements"} href={"/"} altHref={"#products-list-container"}/>
+                                <NavLink name={"About Us"} href={"/about-us"}/>
+                                <NavLink name={"Manage Store"} href={"/store"}/>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
