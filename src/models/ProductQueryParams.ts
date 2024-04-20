67d43bc7a26b0f9dcaece620e4ec123357717143
EricCatayas <catayasericjay@gmail.com>
2024-04-20 21:43:56 +0800

    Modified ProductQueryParams to be undefined and nullable

--- src/models/ProductQueryParams.ts
+++ src/models/ProductQueryParams.ts
@@ -1,10 +1,11 @@
 type ProductQueryParams = {
-  product_name: string | null;
-  store_Id: number | null;
-  category_Id: number | null;
-  min_price: number | null;
-  max_price: number | null;
-  is_negotiable: boolean | null;
+  product_name?: string | null;
+  category_Id?: number | null;
+  min_price?: number | null;
+  max_price?: number | null;
+  is_negotiable?: boolean | null;
+  quantity_Unit?: string | null;
+  store_Id?: number | null;
 };
 
 export default ProductQueryParams;
