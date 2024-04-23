e14b8e9d0144449f03cb19081b9ce55b1f6ded7b
EricCatayas <catayasericjay@gmail.com>
2024-04-23 11:39:19 +0800

    Added success, info, warning, error Notification action creators

--- src/redux/notification/notification.actions.ts
+++ src/redux/notification/notification.actions.ts
@@ -6,6 +6,22 @@ export const addNotification = (message: string, type: NotificationType ) => {
     return createAction(NOTIFICATION_ACTION_TYPES.ADD_NOTIFICATION, {message, type});
 }
 
+export const addSuccessNotification = (message: string) => {
+    return addNotification(message, NotificationType.success)
+}
+
+export const addInfoNotification = (message: string) => {
+    return addNotification(message, NotificationType.info);
+}
+
+export const addWarningNotification = (message: string) => {
+    return addNotification(message, NotificationType.warning);
+}
+
+export const addErrorNotification = (message: string) => {
+    return addNotification(message, NotificationType.error);
+}
+
 export const clearNotifications = () => {
     return createAction(NOTIFICATION_ACTION_TYPES.CLEAR_NOTIFICATION);
 }
