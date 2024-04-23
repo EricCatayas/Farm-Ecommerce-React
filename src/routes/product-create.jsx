3330405c5b9abfeaad0aa1548beb5bb1a4770c6e
EricCatayas <catayasericjay@gmail.com>
2024-04-23 11:37:23 +0800

    Defined the ProductCreate route

---
+++ src/routes/product-create.jsx
@@ -0,0 +1,7 @@
+import ProductCreateDirectory from "../components/directory/product-create.directory";
+
+const ProductCreate = () => {
+  return <ProductCreateDirectory />;
+};
+
+export default ProductCreate;
