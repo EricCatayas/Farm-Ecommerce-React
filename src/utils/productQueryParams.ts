2a707b1f100def67132655ad0d133f01a738ae02
EricCatayas <catayasericjay@gmail.com>
2024-05-02 11:00:54 +0800

    Fixed result definition error in productQueryParams.ts

--- src/utils/productQueryParams.ts
+++ src/utils/productQueryParams.ts
@@ -4,7 +4,15 @@ import ProductQueryParams from "../models/ProductQueryParams";
 export function createProductQueryParams(fieldName: keyof ProductQueryParams, value: any
 ): ProductQueryParams {
   try {
-    const result: ProductQueryParams = {};
+    const result: ProductQueryParams = {
+      product_name: null,
+      category_Id: null,
+      min_price: null,
+      max_price: null,
+      is_negotiable: null,
+      quantity_Unit: null,
+      store_Id: null,
+    };
     result[fieldName] = value;
     return result;
   } catch (error: any) {
