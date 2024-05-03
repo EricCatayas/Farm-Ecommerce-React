c54899bf6a1f44df7822bdc099cb3ec8905c080a
EricCatayas <catayasericjay@gmail.com>
2024-05-03 12:48:18 +0800

    Updated sidebar component design for Agri-Store

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -9,63 +9,51 @@ const Sidebar = () => {
         setToggleSidebar(false);
     }
 
-    return(
-        <>
-            { showSidebar ? (
-
-                <div class="mobile-show" id="mobile-menu">
-                    <div class="overlay">&nbsp;</div>
-                    <div class="header-menu">
-                        <div class="logo-container">
-                            <a href="/">
-                                <img src="/bundles/systemextmain/img/logo-agro.png?122" alt="Agro-market24.pl"/>
-                                <h3 class="logo-text">International Agricultural Exchange</h3>
-                            </a>
-                        </div>
-                        {/* <ul class="nav navbar-nav main-top-nav ">            
-                            <li class="new_pub_link disable_active first last">            
+    return (
+      <>
+        {showSidebar ? (
+          <div className="mobile-show" id="mobile-menu">
+            <div className="header-menu theme-color">
+              <div className="logo-container theme-color">
+                <h2>Agri-Store</h2>
+                <h3>Online Agricultural Exchange Platform</h3>
+              </div>
+              {/* <ul className="nav navbar-nav main-top-nav ">            
+                            <li className="new_pub_link disable_active first last">            
                                 <a href="/pub/new">
                                     Add an advertisement
                                 </a>                            
                             </li>
                         </ul> */}
-                    </div>
-                    <div class="lists">
-                        <span class="menu-cross" onClick={() => toggleSidebar()}></span>
-                        <ul class="nav navbar-nav main-top-nav ">
-                            <li class="navbar-menu-pub-list first active">            
-                                <a href="/">
-                                    Advertisement list
-                                </a>        
-                            </li>
-                            <li>            
-                                <a href="/companies">
-                                    Company catalogue
-                                </a>        
-                            </li>
-                            <li class="disable_active">            
-                                <a href="/register/?referer=chat_menu">
-                                    Messenger
-                                </a>        
-                            </li>
-                            <li class="about_us_menu_link">            
-                                <a href="/about-us">
-                                    About us
-                                </a>        
-                            </li>
-                            <li class="login-link last">            
-                                <a href="/login">
-                                    Sign in
-                                </a>        
-                            </li>
-                            
-                        </ul>
-                    </div>
-                </div>
-            ) : (
-                ""
-            )}
-        </>
+            </div>
+            <div className="lists">
+              <span
+                className="menu-cross"
+                onClick={() => toggleSidebar()}
+              ></span>
+              <ul className="nav navbar-nav main-top-nav ">
+                <li className="navbar-menu-pub-list first active">
+                  <a href="/">Advertisement list</a>
+                </li>
+                <li>
+                  <a href="/about-us">About Us</a>
+                </li>
+                <li className="disable_active">
+                  <a href="/store">Manage Store</a>
+                </li>
+                <li className="disable_active">
+                  <a href="https://farmecommerce.azurewebsites.net" target="_blank">API</a>
+                </li>
+                <li className="login-link last">
+                  <a href="/login">Sign in</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </>
     );
 }
 
