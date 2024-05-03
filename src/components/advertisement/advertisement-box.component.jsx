7489ca62d42488761aa6c66d21f8666c96b52440
EricCatayas <catayasericjay@gmail.com>
2024-05-03 17:22:56 +0800

    Modified z-index of .alert element in AdvertisementBox component

--- src/components/advertisement/advertisement-box.component.jsx
+++ src/components/advertisement/advertisement-box.component.jsx
@@ -1,13 +1,10 @@
-import "./advertisement-box.styles.css";
 
 const AdvertisementBox = () => {
     return (
-      <div className="alert alert-info advertisement-box">
+      <div className="alert alert-info" style={{ zIndex: 10 }}>
         <div className="row">
           <div className="col-xs-8">
-            <h4>
-              Would you like to advertise your products?
-            </h4>
+            <h4>Would you like to advertise your products?</h4>
           </div>
           <div className="col-xs-4">
             <a href="/product/create" className="btn btn-theme mt-12">
