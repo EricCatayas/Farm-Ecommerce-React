9d031a794e0fa4548428618f552d0fad0926da97
EricCatayas <catayasericjay@gmail.com>
2024-05-06 12:43:36 +0800

    Implemented dropdown component test

---
+++ tests/components/dropdown/dropdown.component.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Dropdown from "../../../src/components/dropdown/dropdown.component";
+
+describe("Dropdown component", () => {
+
+    const testTitle = "test";
+    const testName = "test";
+    const testArgs =  [
+        {id:1, name:"element 1"}, 
+        {id:2, name:"element 2"},
+        {id:3, name:"element 3"}
+    ];
+
+    it("renders dropdown correctly", () => {
+        render(<Dropdown args={testArgs} selectTitle={testTitle} name={testName} onChange={() => {}} />);
+
+        testArgs.forEach((element) => {
+            expect(screen.getByText(element.name));
+        })
+
+        expect(screen.getByText(testTitle)).toBeInTheDocument();
+    });
+});
