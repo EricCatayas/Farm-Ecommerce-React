f69a00c625f4941e82d99a7c72e4905c37aa358f
EricCatayas <catayasericjay@gmail.com>
2024-05-07 12:12:54 +0800

    Fixed ProductCategories component test implementation

--- tests/components/product-categories/product-categories.component.test.tsx
+++ tests/components/product-categories/product-categories.component.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../../src/redux/store"; // Import your Redux store
@@ -6,10 +5,10 @@ import ProductCategory from "../../../src/models/ProductCategory";
 import { ProductCategoriesState } from "../../../src/redux/productCategory/productCategories.reducer";
 import { renderWithProviders } from "../../test.utils";
 import ProductCategories from "../../../src/components/category-container/product-categories.component"; // Import the component you're testing
+import React from "react";
 
 test("renders subcomponents of ProductCategories", () => {
-
-  const initialCategories: ProductCategory[] = [ 
+  const initialCategories: ProductCategory[] = [
     {
       id: 1,
       name: "Category A",
@@ -18,16 +17,16 @@ test("renders subcomponents of ProductCategories", () => {
           id: 11,
           name: "SubCategory A1",
           subcategories: [],
-          image_Url: ""
+          image_Url: "",
         },
         {
           id: 12,
           name: "SubCategory A2",
           subcategories: [],
-          image_Url: ""
-        }
+          image_Url: "",
+        },
       ],
-      image_Url: ""
+      image_Url: "",
     },
     {
       id: 2,
@@ -37,26 +36,29 @@ test("renders subcomponents of ProductCategories", () => {
           id: 21,
           name: "SubCategory B1",
           image_Url: "",
-          subcategories: []
+          subcategories: [],
         },
         {
           id: 22,
           name: "SubCategory B2",
           subcategories: [],
-          image_Url: ""
-        }
+          image_Url: "",
+        },
       ],
-      image_Url: ""
-    }
+      image_Url: "",
+    },
   ];
 
   const categoriesInitialState: ProductCategoriesState = {
     productCategories: initialCategories,
     isLoading: false,
-    error: null
-  }
+    error: null,
+  };
 
-  const { getByText } = renderWithProviders(<ProductCategories onSelectEventHandler={() => {}}/>, { preloadedState: { productCategories: categoriesInitialState }});
+  const { getByText } = renderWithProviders(
+    <ProductCategories onSelectEventHandler={() => {}} />,
+    { preloadedState: { productCategories: categoriesInitialState } }
+  );
 
   // Check if the subcomponents are rendered in the component
   initialCategories.forEach((category) => {
