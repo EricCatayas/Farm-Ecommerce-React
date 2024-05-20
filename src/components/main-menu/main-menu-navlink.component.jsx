16a5685feb353b94e5099da37452b5febdc833ec
EricCatayas <catayasericjay@gmail.com>
2024-05-20 09:44:19 +0800

    fix: replaced href redirect with useNavigate hook in MainMenuNavLink

--- src/components/main-menu/main-menu-navlink.component.jsx
+++ src/components/main-menu/main-menu-navlink.component.jsx
@@ -1,17 +1,19 @@
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; 
 
 const NavLink = ({ name, href}) => {
     const location = useLocation();
+    const navigate = useNavigate();
     const currentPath = location.pathname;
 
     const handleClick = (e) => {
-        if(currentPath === href)
-            e.preventDefault();
+        e.preventDefault();        
+        navigate(href);
     }
 
     return (
         <li className="nav-item">
-            <a className={currentPath == href ? "active" : ""} href={href} onClick={handleClick}>{name}</a>
+            <a className={currentPath == href ? "active" : ""} onClick={handleClick}>{name}</a>
         </li>
     )
 };
