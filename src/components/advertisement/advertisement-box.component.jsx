e448ba535a759f26e13a6811699b3542a78ccb57
EricCatayas <catayasericjay@gmail.com>
2024-05-20 10:04:28 +0800

    fix: replaced hyperlink href with useNavigate hook to redirect

--- src/components/advertisement/advertisement-box.component.jsx
+++ src/components/advertisement/advertisement-box.component.jsx
@@ -1,19 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 
 const AdvertisementBox = () => {
-    return (
-      <div className="alert alert-info" style={{ zIndex: 10 }}>
-        <div className="row">
-          <div className="col-xs-8">
-            <h4>Would you like to advertise your products?</h4>
-          </div>
-          <div className="col-xs-4">
-            <a href="/product/create" className="btn btn-theme mt-12">
-              ADD AN ADVERTISEMENT FOR FREE
-            </a>
-          </div>
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate("/product/create");
+  }
+
+  return (
+    <div className="alert alert-info" style={{ zIndex: 10 }}>
+      <div className="row">
+        <div className="col-xs-8">
+          <h4>Would you like to advertise your products?</h4>
+        </div>
+        <div className="col-xs-4">
+          <a className="btn btn-theme mt-12" onClick={handleClick}>
+            ADD AN ADVERTISEMENT FOR FREE
+          </a>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default AdvertisementBox;
