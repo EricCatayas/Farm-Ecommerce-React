307530d3b89f67aaf71b8572a1dcbf61b2d85fab
EricCatayas <catayasericjay@gmail.com>
2024-05-22 15:47:45 +0545

    test: added data-testid attribute in MainMenu component

--- src/components/main-menu/main-menu.component.jsx
+++ src/components/main-menu/main-menu.component.jsx
@@ -3,28 +3,28 @@ import NavLink from './main-menu-navlink.component';
 
 
 const MainMenu = () => {    
-    return(
-        <section className="main-menu text-center">
-            <div className="container">
-                <div className="row">
-                    <div className="col col-xs-3">
-                        <div className="logo-container">
-                            <img src="/logo-agri-store.png"/>
-                        </div>
-                    </div>
-                    <div className="col col-xs-9 mobile-hide">
-                        <div className="nav-container">
-                            <ul className="nav navbar-nav d-flex flex-row">
-                                <NavLink name={"Advertisements"} href={"/"}/>
-                                <NavLink name={"About Us"} href={"/about-us"}/>
-                                <NavLink name={"Manage Store"} href={"/store"}/>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+    return (
+      <section className="main-menu text-center" data-testid="main-menu">
+        <div className="container">
+          <div className="row">
+            <div className="col col-xs-3">
+              <div className="logo-container">
+                <img src="/logo-agri-store.png" />
+              </div>
             </div>
-        </section>
-    )
+            <div className="col col-xs-9 mobile-hide">
+              <div className="nav-container">
+                <ul className="nav navbar-nav d-flex flex-row">
+                  <NavLink name={"Advertisements"} href={"/"} />
+                  <NavLink name={"About Us"} href={"/about-us"} />
+                  <NavLink name={"Manage Store"} href={"/store"} />
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
 }
 
 export default MainMenu;
