7c00e3900aba009f15f31a10a999a8742b9043c1
EricCatayas <catayasericjay@gmail.com>
2024-05-22 15:44:55 +0545

    test: reimplemented msw handlers

--- tests/mocks/handlers.js
+++ tests/mocks/handlers.js
@@ -9,10 +9,24 @@ dotenv.config();
 // when receiving a get request to the `/api/user` endpoint
 export const handlers = [
   http.get(
-    `${process.env.REACT_APP_FARM_ECOMMERCE_URL}/api/v1/Products/Get?Id=${mockProduct.id}`,
-    async () => {
-      await delay(150);
-      return HttpResponse.json(mockProduct);
+    `${process.env.REACT_APP_FARM_ECOMMERCE_URL}/api/v1/Products/Get`,
+    async ({request}) => {
+
+      const url = new URL(request.url);
+      // Read the "id" URL query parameter using the "URLSearchParams" API.
+      // Given "/product?id=1", "productId" will equal "1".
+      const productId = url.searchParams.get("id");
+
+      // Note that query parameters are potentially undefined.
+      // Make sure to account for that in your handlers.
+      
+      if (productId && productId == mockProduct.id) {
+        await delay(150);
+        return HttpResponse.json(mockProduct);
+      }
+      else {
+        return new HttpResponse(null, { status: 404 });
+      }
     }
   ),
 ];
