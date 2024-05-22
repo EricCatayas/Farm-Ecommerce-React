1e4fc52d9bcc951ed6f9db586dd31a0263852db7
EricCatayas <catayasericjay@gmail.com>
2024-05-22 15:50:01 +0545

    style: improved indentation in ProductCreateDirectory

--- src/components/directory/product-create.directory.jsx
+++ src/components/directory/product-create.directory.jsx
@@ -5,197 +5,197 @@ import { convertProductToFormData } from "../../utils/formData";
 import { multipartPostRequestAsync } from "../../utils/multipartPostRequest.utils";
 
 const productCreateFormFields = {
-    Name : '',
-    Description : '',
-    Price: null,
-    Category_Id : null,
-    Quantity_Unit: '',
-    Qty_In_Stock : null,
-    Is_Negotiable: false,
-    Image_Files : null,
+  Name : '',
+  Description : '',
+  Price: null,
+  Category_Id : null,
+  Quantity_Unit: '',
+  Qty_In_Stock : null,
+  Is_Negotiable: false,
+  Image_Files : null,
 }
 
 //TODO
 const ProductCreateDirectory = () =>  {
     
-    const [formFields, setFormFields] = useState(productCreateFormFields);
+  const [formFields, setFormFields] = useState(productCreateFormFields);
 
-    //TODO
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      try{
-        const formData = convertProductToFormData(formFields);
+  //TODO
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try{
+      const formData = convertProductToFormData(formFields);
 
-        const response = await multipartPostRequestAsync(formData, 
-          "/api/v1/Products/Create",
-          (data) => { 
-              console.log("Product Create response:\n"); 
-              console.log(data);
-          },
-          (error)=>{ 
-              console.error(error);
-          }
-        );              
-      }
-      catch(error){
-          console.log(error.message);
-      }
+      const response = await multipartPostRequestAsync(formData, 
+        "/api/v1/Products/Create",
+        (data) => { 
+            console.log("Product Create response:\n"); 
+            console.log(data);
+        },
+        (error)=>{ 
+            console.error(error);
+        }
+      );              
     }
-
-    const textInputChangeHandler = async (event) => {
-        const { name, value } = event.target;
-        setFormFields({...formFields, [name]:value});
+    catch(error){
+        console.log(error.message);
     }
-    const numberInputChangeHandler = async (event) => {
-        const { name, value } = event.target;
-        const numValue = parseInt(value); 
-        setFormFields({...formFields, [name]:numValue});
-    }
-    const checkBoxChangeHandler = async (event) => {
-        const { name, checked } = event.target;
-        setFormFields({...formFields, [name]:checked});
-    }
-    const categorySelectEventHandler = (categoryId) => {
-      setFormFields((prevFormFields) => ({
-        ...prevFormFields,
-        Category_Id: categoryId,
-      }));
-    };
-    const handleFileInputChange = (event) => {
-      const selectedFiles = event.target.files;
+  }
 
-      setFormFields({
-        ...formFields,
-        Image_Files: selectedFiles,
-      });
-    };
+  const textInputChangeHandler = async (event) => {
+      const { name, value } = event.target;
+      setFormFields({...formFields, [name]:value});
+  }
+  const numberInputChangeHandler = async (event) => {
+      const { name, value } = event.target;
+      const numValue = parseInt(value); 
+      setFormFields({...formFields, [name]:numValue});
+  }
+  const checkBoxChangeHandler = async (event) => {
+      const { name, checked } = event.target;
+      setFormFields({...formFields, [name]:checked});
+  }
+  const categorySelectEventHandler = (categoryId) => {
+    setFormFields((prevFormFields) => ({
+      ...prevFormFields,
+      Category_Id: categoryId,
+    }));
+  };
+  const handleFileInputChange = (event) => {
+    const selectedFiles = event.target.files;
 
-    console.log("Form Fields in Product Create");
-    console.log(formFields);
+    setFormFields({
+      ...formFields,
+      Image_Files: selectedFiles,
+    });
+  };
 
-    return (
-      <section className="product-create-container">
-        <div className="container">
-          <form onSubmit={handleSubmit} enctype="multipart/form-data">
-            <div className="row mb-3">
-              <div className="col">
-                <DefaultPanel heading={"1. Category"} />
-                <p>Selected Category: {formFields.Category_Id}</p>
-                <ProductCategories
-                  onSelectEventHandler={categorySelectEventHandler}
-                />
-              </div>
+  console.log("Form Fields in Product Create");
+  console.log(formFields);
+
+  return (
+    <section className="product-create-container">
+      <div className="container">
+        <form onSubmit={handleSubmit} enctype="multipart/form-data">
+          <div className="row mb-3">
+            <div className="col">
+              <DefaultPanel heading={"1. Category"} />
+              <p>Selected Category: {formFields.Category_Id}</p>
+              <ProductCategories
+                onSelectEventHandler={categorySelectEventHandler}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <DefaultPanel heading={"2. Product"} />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <DefaultPanel heading={"2. Product"} />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <input
-                  type="file"
-                  className="form-control"
-                  name="image_Files"
-                  multiple
-                  onChange={handleFileInputChange}
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="file"
+                className="form-control"
+                name="image_Files"
+                multiple
+                onChange={handleFileInputChange}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Name"
-                  placeholder="Name"
-                  required
-                  onChange={textInputChangeHandler}
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                name="Name"
+                placeholder="Name"
+                required
+                onChange={textInputChangeHandler}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <textarea
-                  type="text"
-                  className="form-control"
-                  name="Description"
-                  placeholder="Description"
-                  onChange={textInputChangeHandler}
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <textarea
+                type="text"
+                className="form-control"
+                name="Description"
+                placeholder="Description"
+                onChange={textInputChangeHandler}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <input
-                  type="number"
-                  className="form-control"
-                  name="Price"
-                  placeholder="Price"
-                  required
-                  onChange={numberInputChangeHandler}
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="number"
+                className="form-control"
+                name="Price"
+                placeholder="Price"
+                required
+                onChange={numberInputChangeHandler}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Quantity_Unit"
-                  placeholder="(e.g. ton, kilo, piece, etc.)"
-                  required
-                  onChange={textInputChangeHandler}
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                name="Quantity_Unit"
+                placeholder="(e.g. ton, kilo, piece, etc.)"
+                required
+                onChange={textInputChangeHandler}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <input
-                  type="number"
-                  className="form-control"
-                  name="Qty_In_Stock"
-                  placeholder="Quantity In Stock"
-                  onChange={numberInputChangeHandler}
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="number"
+                className="form-control"
+                name="Qty_In_Stock"
+                placeholder="Quantity In Stock"
+                onChange={numberInputChangeHandler}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col-auto">
-                <label className="form-check-label">
-                  Is The Price Negotiable?
-                </label>
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  name="Is_Negotiable"
-                  placeholder="Negotiable"
-                  onChange={checkBoxChangeHandler}
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col-auto">
+              <label className="form-check-label">
+                Is The Price Negotiable?
+              </label>
+              <input
+                type="checkbox"
+                className="form-check-input"
+                name="Is_Negotiable"
+                placeholder="Negotiable"
+                onChange={checkBoxChangeHandler}
+              />
             </div>
+          </div>
 
-            <div className="row mb-3">
-              <div className="col">
-                <input
-                  className="btn btn-primary"
-                  type="submit"
-                  value="Add Advertisement"
-                />
-              </div>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Add Advertisement"
+              />
             </div>
-          </form>
-        </div>
-      </section>
-    );    
+          </div>
+        </form>
+      </div>
+    </section>
+  );    
     
 }
 
